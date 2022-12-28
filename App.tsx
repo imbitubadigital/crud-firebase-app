aae7de5bda501e6dff134901b3915bcd4d182c76
Antonio Fernandes <imbitubadigital@gmail.com>
2022-12-28 08:15:25 -0300

    install styled components and navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+import * as S from './styles'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <S.Container>
+      <S.Txt>Meu app</S.Txt>
       <StatusBar style="auto" />
-    </View>
+    </S.Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+flex: 1;
+justify-content: center;
+align-items: center;
+background: #ccc;
+`;
+
+export const Txt = styled.Text`
+color: red;
+`;
